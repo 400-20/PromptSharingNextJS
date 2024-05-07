36270003e8ae2950b6650be471aba9c59b049375
abhishek <as77468@gmail.com>
2024-05-07 13:06:38 +0530

    fixes

--- utils/database.js
+++ utils/database.js
@@ -1,28 +1,24 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
 let isConnected = false;
 
-
 export const connectToDB = async () => {
-    mongoose.set('strictQuery', true)
-}
+    if (isConnected) {
+        console.log("MongoDB is already connected");
+        return;
+    }
 
-if(isConnected){
-    console.log("MongoDB is already connected");
-}
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: 'share_prompt',
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
 
-try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-        dbName: 'share_prompt',
-        useNewUrlParser: true, 
-        useUnifiedTopology: true,
-        
-    })
-
-    isConnected = true
-    console.log("MongoDB connected")
-    
-} catch (error) {
-    console.log(error);
-    
-}
+        isConnected = true;
+        console.log("MongoDB connected");
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        throw error; // You can choose to handle or propagate the error as needed
+    }
+};
